ce469002460ac6770badee222bbe0f4a0564e28b
arif99050 <arifulislam99050@gmail.com>
2022-04-07 14:01:58 +0600

    work with onBlur and onChange event handler

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ import app from "./firebase.init";
 const auth = getAuth(app);
 
 function App() {
+  const handleOnBlur=(event)=>{
+    console.log(event.target.value);
+  }
+  const handleOnChange=(event)=>{
+    event.preventDefault();
+  }
   return (
     <div className="App">
-      <form action="" style={{margin:"50px"}}>
-        <input style={{width:"500px", height:"20px", padding:"10px"}} type="email" name="" id="" placeholder="enter your email"  /> <br /> <br />
-        <input style={{width:"500px", height:"20px",padding:"10px"}} type="password" name="" id="" placeholder="enter your password"/> <br />
+      <form onChange={handleOnChange} action="" style={{margin:"50px"}}>
+        <input onBlur={handleOnBlur} style={{width:"500px", height:"20px", padding:"10px"}} type="email" name="" id="" placeholder="enter your email"  /> <br /> <br />
+        <input onBlur={handleOnBlur} style={{width:"500px", height:"20px",padding:"10px"}} type="password" name="" id="" placeholder="enter your password"/> <br />
         <input style={{width:"100px", height:"20px", margin:"20px"}} type="submit" value="Sign In" />
         
       </form>
